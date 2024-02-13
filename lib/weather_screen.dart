d645f14937007ce8f6fb3cf00406116a8b4e8748
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-02-14 05:19:00 +0530

    add appBar to the Scaffold

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -5,6 +5,8 @@ class WeatherScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
