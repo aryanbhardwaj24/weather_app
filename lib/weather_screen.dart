879caf53d9b702bcf5457d006fdab2fd1125bfa9
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-02-17 01:38:18 +0530

    extract Column widget representing the Humidity part in the additional info section to be a class named AdditionalInfoItem in order to enhance code modularity and add two more instances of it to the children of Row in the additional info section

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -126,33 +126,46 @@ class WeatherScreen extends StatelessWidget {
 
             const Row(
               children: [
-                Column(
-                  children: [
-                    Icon(
-                      Icons.water_drop,
-                      size: 32,
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Text('Humidity'),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      '94',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                )
+                AdditionalInfoItem(),
+                AdditionalInfoItem(),
+                AdditionalInfoItem(),
               ],
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class AdditionalInfoItem extends StatelessWidget {
+  const AdditionalInfoItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Icon(
+          Icons.water_drop,
+          size: 32,
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text('Humidity'),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '94',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 }
