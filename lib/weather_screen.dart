51341b04d2fb974cda872856056a0b643a0cd08d
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-02-16 17:18:50 +0530

    copy the Card widget of the first weather forecast card four times in order to have five weather forecast cards in total and update the use of const accordingly to improve code efficiency

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -103,8 +103,144 @@ class WeatherScreen extends StatelessWidget {
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(12),
                     ),
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(
+                      children: [
+                        Text(
+                          '09:00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          '320.12 K',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(
+                      children: [
+                        Text(
+                          '09:00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          '320.12 K',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(
+                      children: [
+                        Text(
+                          '09:00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          '320.12 K',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(
+                      children: [
+                        Text(
+                          '09:00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          '320.12 K',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(
                       children: [
                         Text(
                           '09:00',
