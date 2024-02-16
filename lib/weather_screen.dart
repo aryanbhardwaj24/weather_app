b328cbf9d0ed40e2a7d44307704288d9f85e9f7f
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-02-16 17:09:18 +0530

    remove the SizedBox wrapping of the Card widget representing the first weather forecast card and add set the width of the Container wrapping the child of this Card widget since we want to apply the custom width to the contents of this Card widget and not to the whole Card widget

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -96,39 +96,37 @@ class WeatherScreen extends StatelessWidget {
 
             Row(
               children: [
-                SizedBox(
-                  width: 100,
-                  child: Card(
-                    elevation: 6,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      padding: EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            '09:00',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          '09:00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
                           ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                          Icon(
-                            Icons.cloud,
-                            size: 32,
-                          ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                          Text(
-                            '320.12 K',
-                          ),
-                        ],
-                      ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          '320.12 K',
+                        ),
+                      ],
                     ),
                   ),
                 ),
